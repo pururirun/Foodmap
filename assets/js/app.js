b6d3fb3c1472f24494bf8342025c9bcd879569d1
pururirun <j.mardonescava@gmail.com>
2017-12-14 16:04:33 -0300

    Modificación

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,33 @@
 $(function(){
    setTimeout(function() {
       $('#splash').fadeOut(100);
-   }, 1000);
+   }, 3000);
 });
 
 
-$(document).ready(function(){
+
     $('.zoom').hover(function() {
         $(this).addClass('transition');
     }, function() {
         $(this).removeClass('transition');
     });
-});
+
 
 /*-------------Modal---------------------*/
-$(document).ready(function(){
     $('#open').click(function(){
         $('.modal').toggle('slow');
     });
     $('.modal').click(function(){
       $(this).css({display:"none"});
     });
-  });
+
 
   /*-----filtro------*/
 
-  $(document).ready(function(){
+
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $("#myList div").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
-});
